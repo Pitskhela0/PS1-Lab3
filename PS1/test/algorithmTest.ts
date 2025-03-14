6520054e4550e93070de50d7822f5f9ec2eb2de7
Gakha Pitskhelauri <120589386+Pitskhela0@users.noreply.github.com>
2025-03-14 12:18:47 +0400

    added file

---
+++ PS1/test/algorithmTest.ts
@@ -0,0 +1,118 @@
+import assert from "assert";
+import { AnswerDifficulty, Flashcard, BucketMap } from "../src/flashcards";
+import {
+  toBucketSets,
+  getBucketRange,
+  practice,
+  update,
+  getHint,
+  computeProgress,
+  isPrime,
+} from "../src/algorithm";
+
+/*
+ * Testing strategy for toBucketSets():
+ *
+ * TODO: Describe your testing strategy for toBucketSets() here.
+ */
+describe("toBucketSets()", () => {
+  // input is Map<Bucket> 
+  // 0:Set<Flashcard>,   1: Set<Flashcard> , ....
+  //
+  // [0,1,2,3,4, ....]
+  // [Set<Flashcard>, Set<Flashcard>, ....]
+  // 
+  let test1  = new Map<Number, Set<Flashcard>>;
+
+  test1.set(1, new Set.of)
+
+
+
+
+  it("Example test case - replace with your own tests", () => {
+    assert.fail(
+      "Replace this test case with your own tests based on your testing strategy"
+    );
+  });
+});
+
+/*
+ * Testing strategy for getBucketRange():
+ *
+ * TODO: Describe your testing strategy for getBucketRange() here.
+ */
+describe("getBucketRange()", () => {
+  it("Example test case - replace with your own tests", () => {
+    assert.fail(
+      "Replace this test case with your own tests based on your testing strategy"
+    );
+  });
+});
+
+/*
+ * Testing strategy for practice():
+ *
+ * TODO: Describe your testing strategy for practice() here.
+ */
+describe("practice()", () => {
+  it("Example test case - replace with your own tests", () => {
+    assert.fail(
+      "Replace this test case with your own tests based on your testing strategy"
+    );
+  });
+});
+
+/*
+ * Testing strategy for update():
+ *
+ * TODO: Describe your testing strategy for update() here.
+ */
+describe("update()", () => {
+  it("Example test case - replace with your own tests", () => {
+    assert.fail(
+      "Replace this test case with your own tests based on your testing strategy"
+    );
+  });
+});
+
+/*
+ * Testing strategy for getHint():
+ *
+ * TODO: Describe your testing strategy for getHint() here.
+ */
+describe("getHint()", () => {
+  it("Example test case - replace with your own tests", () => {
+    assert.fail(
+      "Replace this test case with your own tests based on your testing strategy"
+    );
+  });
+});
+
+/*
+ * Testing strategy for computeProgress():
+ *
+ * TODO: Describe your testing strategy for computeProgress() here.
+ */
+describe("computeProgress()", () => {
+  it("Example test case - replace with your own tests", () => {
+    assert.fail(
+      "Replace this test case with your own tests based on your testing strategy"
+    );
+  });
+});
+
+
+
+/*
+ * Testing strategy for isPrime():
+ *
+ * TODO: Describe your testing strategy for computeProgress() here.
+ */
+describe("isPrime()", () => {
+  it("Example test case - replace with your own tests", () => {
+    assert.strictEqual(isPrime(4),false);
+  });
+  it("Example test case - replace with your own tests", () => {
+    assert.strictEqual(isPrime(5),true);
+  });
+});
